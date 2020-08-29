2b45de86379cd6873c213cdacfeaca1f46f775a4
9527bb <56328536+9527bb@users.noreply.github.com>
2020-08-29 15:54:24 +0800

    1

--- src/main/java/com/glb/springbootDemo/SpringbootDemoApplication.java
+++ src/main/java/com/glb/springbootDemo/SpringbootDemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootDemoApplication {
 
-	public static void main(String[] a) {
-		SpringApplication.run(SpringbootDemoApplication.class, a);
+	public static void main(String[] ab) {
+		SpringApplication.run(SpringbootDemoApplication.class, ab);
 	}
 	
 	public void ss(){
